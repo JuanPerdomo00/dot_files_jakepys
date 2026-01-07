e678ee68a536c9587e2103bd0e865ae63e42c09c
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-01-06 20:37:54 -0500

    Update

--- dot_config/river/autostart_app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-pkill -SIGTERM kanshi
-pkill -SIGTERM waybar
-
-riverctl spawn "kanshi &"
-riverctl spawn "waybar &"
-riverctl spawn "swaybg -m fill -i $HOME/.config/wallpapers/arch-waffiu.jpg &"
--- install.sh
+++ install.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/bash
-
-
-DOTFILES_DIR="$HOME/jdotfiles"
+DOTFILES_DIR="$HOME/dot_files_jakepys"
 
 link() {
     local src="$1"
     local dest="$2"
     local name="$3"
     
+    if [ ! -e "$src" ]; then
+        echo "✗ Error: $name - $src no existe"
+        return 1
+    fi
+    
     mkdir -p "$(dirname "$dest")"
     
+    [ -L "$dest" ] && rm "$dest"
+    
     ln -sf "$src" "$dest"
-    echo "Add config $name"
+    echo "✓ $name"
 }
 
+[ ! -d "$DOTFILES_DIR" ] && echo "Error: $DOTFILES_DIR no existe" && exit 1
+
 link "$DOTFILES_DIR/dot_config/nvim" "$HOME/.config/nvim" "Neovim"
-link  "$DOTFILES_DIR/dot_config/ghostty" "$HOME/.config/ghostty" "Terminal ghostty"
-link  "$DOTFILES_DIR/dot_config/fastfetch" "$HOME/.config/fastfetch" "Fastfetch"
-link  "$DOTFILES_DIR/dot_config/rofi" "$HOME/.config/rofi" "rofi"
-link  "$DOTFILES_DIR/dot_config/starship" "$HOME/.config/starship" "starship"
-link  "$DOTFILES_DIR/dot_config/niri" "$HOME/.config/niri" "niri"
-link  "$DOTFILES_DIR/dot_config/waybar" "$HOME/.config/waybar" "waybar"
-link  "$DOTFILES_DIR/dot_config/river" "$HOME/.config/river" "river"
-link  "$DOTFILES_DIR/dot_config/kanshi" "$HOME/.config/kanshi" "kanshi"
-link  "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc" "zsh config"
-link  "$DOTFILES_DIR/.alias_zsh" "$HOME/.alias_zsh" "alias config"
-link  "$DOTFILES_DIR/.function_zsh" "$HOME/.function_zsh" "function config"
-link  "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig" ".gitconfig config"
-link  "$DOTFILES_DIR/start_river" "$HOME/start_river" "River"
+link "$DOTFILES_DIR/dot_config/ghostty" "$HOME/.config/ghostty" "Ghostty"
+link "$DOTFILES_DIR/dot_config/fastfetch" "$HOME/.config/fastfetch" "Fastfetch"
+link "$DOTFILES_DIR/dot_config/rofi" "$HOME/.config/rofi" "Rofi"
+link "$DOTFILES_DIR/dot_config/starship" "$HOME/.config/starship" "Starship"
+link "$DOTFILES_DIR/dot_config/niri" "$HOME/.config/niri" "Niri"
+link "$DOTFILES_DIR/dot_config/waybar" "$HOME/.config/waybar" "Waybar"
+link "$DOTFILES_DIR/dot_config/swaylock" "$HOME/.config/swaylock" "Swaylock"
+link "$DOTFILES_DIR/dot_config/starship.toml" "$HOME/.config/starship.toml" "Swaylock"
+link "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc" "Zsh"
+link "$DOTFILES_DIR/.alias_zsh" "$HOME/.alias_zsh" "Alias"
+link "$DOTFILES_DIR/.function_zsh" "$HOME/.function_zsh" "Functions"
+link "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig" "Git"
+link "$DOTFILES_DIR/wallpapers" "$HOME/.config/wallpapers" "Wallpapers"
 
-# Wallpapers
-link  "$DOTFILES_DIR/wallpapers" "$HOME/.config/wallpapers" "Wallpapers"
-
-
-echo "Good :D"
+echo "Listo :D"
