4915c57c3c6e6965d9f679bd38771a57d023ce0c
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-01-13 18:08:47 -0500

    Formated: Ruff format

--- dot_config/waybar/scripts/wanews.py
+++ dot_config/waybar/scripts/wanews.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 #
 # A python script to fetch and display arch linux news for waybar.
-# 
+#
 # Copyright (C) 2025  Jakepys
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
