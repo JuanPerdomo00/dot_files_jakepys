070bc73ebff7d7ba6a5cc9fa9ded0a2f518c94f3
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-01-26 21:07:14 -0500

    Update: Add cowsay msg and new keymaps

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -4,26 +4,36 @@ return {
 		local startify = require("alpha.themes.startify")
 
 		startify.section.header.val = {
-			"     ██╗ █████╗ ██╗  ██╗███████╗██████╗ ██╗   ██╗███████╗███╗   ██╗██╗   ██╗██╗███╗   ███╗    ",
-			"     ██║██╔══██╗██║ ██╔╝██╔════╝██╔══██╗╚██╗ ██╔╝██╔════╝████╗  ██║██║   ██║██║████╗ ████║    ",
-			"     ██║███████║█████╔╝ █████╗  ██████╔╝ ╚████╔╝ ███████╗██╔██╗ ██║██║   ██║██║██╔████╔██║    ",
-			"██   ██║██╔══██║██╔═██╗ ██╔══╝  ██╔═══╝   ╚██╔╝  ╚════██║██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║    ",
-			"╚█████╔╝██║  ██║██║  ██╗███████╗██║        ██║   ███████║██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║    ",
-			" ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝        ╚═╝   ╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝    ",
-			"                                                                                              ",
-			"With God by your side, no fear can stop you, and no dream is too big. Trust,",
-			"move forward, and never doubt He walks with you.",
+			" _______________________________________",
+			"/ There's nothing better than gnu/linux \\",
+			"\\ and nvim as your ally                 /",
+			" --------------------------------------- ",
+			"        \\   ^__^",
+			"         \\  (oo)\\________",
+			"            (__)\\ Jakepys)\\/\\",
+			"                ||----w |",
+			"                ||     ||",
+			"",
+			"    [b] New Buffer        [0] Config",
 		}
+
+
+
 
 		startify.file_icons.provider = "devicons"
 		startify.section.top_buttons.val = {
-			startify.button("e", "  New file", ":ene <CR>"),
 			startify.button("SPC f f", "󰈞  Find file", ":Telescope find_files<CR>"),
 			startify.button("SPC f h", "  Recently opened files", ":Telescope oldfiles<CR>"),
 			startify.button("SPC f r", "󰃃  Frecency/MRU", ":Telescope frecency<CR>"),
 			startify.button("SPC f g", "  Find word", ":Telescope live_grep<CR>"),
 			startify.button("SPC f m", "  Jump to bookmarks", ":Telescope marks<CR>"),
 			startify.button("SPC s l", "󰑤  Open last session", ":SessionLoad<CR>"),
+		}
+
+
+		startify.section.footer.val = {
+			startify.button("b", "New Buffer", ":enew<CR>"),
+			startify.button("0", "Config", ":e ~/.config/nvim/init.lua<CR>")
 		}
 		require("alpha").setup(startify.config)
 	end,
