6b6381712a8ee6b4213d858a2c32ae88f7cd4d41
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-07-14 14:36:30 -0500

    feat: add D programming Language ✨

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,6 +22,7 @@ vim.lsp.config("lua_ls", {
 
 
 vim.lsp.enable({
+    "serve_d",
     "gleam",
     "ols",
     "rust_analyzer",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ local treesitter = require("nvim-treesitter")
 local ensure_installed = {
     "go", "rust", "typescript", "javascript", "tsx", "gleam",
     "html", "css", "json", "bash",
-    "http", "dockerfile", "c", "python",
+    "http", "dockerfile", "c", "python", "d",
 }
 
 treesitter.install(ensure_installed)
