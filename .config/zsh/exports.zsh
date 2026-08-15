a72839601ca93ccbcdb88b2f483fd5adb872d38b
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-08-15 10:58:42 -0500

    fix: change jakepys name upper to lower 🐛

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -8,11 +8,15 @@ export GOPATH="$HOME/go"
  
 export STARSHIP_CONFIG="$HOME/.config/starship.toml"
  
+export GEM_HOME="$HOME/.local/share/gem/ruby/3.4.0"
+export GEM_PATH="$GEM_HOME"
+export PATH="$GEM_HOME/bin:$PATH"
+
 export PATH="$PATH:\
-/home/Jakepys/.local/bin:\
+/home/jakepys/.local/bin:\
 /usr/local/go/bin:\
-/home/Jakepys/.deno/bin:\
-/home/Jakepys/go/bin:\
+/home/jakepys/.deno/bin:\
+/home/jakepys/go/bin:\
 /home/jakepys/.cargo/bin:\
 $HOME/.zig_versions"
 
