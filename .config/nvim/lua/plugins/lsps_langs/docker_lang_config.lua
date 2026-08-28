ced82e3bb86fd3e8c02764e5f6b4e20cd2efcaaf
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2026-08-28 17:35:21 -0500

    fix: reformat docker format config 🐛

--- .config/nvim/lua/plugins/lsps_langs/docker_lang_config.lua
+++ .config/nvim/lua/plugins/lsps_langs/docker_lang_config.lua
@@ -1,5 +1,14 @@
 return {
     cmd = { "docker-langserver", "--stdio" },
     filetypes = { "dockerfile" },
-    root_markers = { "Dockerfile", ".git" }
+    root_markers = { "Dockerfile", ".git" },
+    settings = {
+        docker = {
+            languageserver = {
+                formatter = {
+                    ignoreMultilineInstructions = true
+                }
+            }
+        }
+    }
 }
